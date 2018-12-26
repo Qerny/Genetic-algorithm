138ed9a45f2ad3a7fc709853505787984a2a0e0e
Qerny <sasha.kogut.98@mail.ru>
2018-12-27 01:01:58 +0300

    added class ExchaustiveSearch

---
+++ TravellingSalesmanProblem/ExhaustiveSearch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravellingSalesmanProblem
+{
+    class ExhaustiveSearch<T>
+    {
+       //Permutation generator
+        private MinArea.IPermutations<T> _perm;
+
+        private IExhaustive<T> _exh;
+
+        //Time checker
+        private ITimeCheck _time;
+
+        public ExhaustiveSearch(
+            MinArea.IPermutations<T> perm, 
+            IExhaustive<T> exh,
+            ITimeCheck time = null)
+        {
+            _perm = perm;
+            _exh = exh;
+            _time = time;
+        }        
+
+        // Gets optimal permutation
+        public T[] GetOptimal()
+        {            
+            T[] result = null;
+            int maxK = 0;
+
+            _time?.Start();
+
+            _perm.Generate(_exh.GetSet(), perm =>
+            {
+                int k = _exh.OptimalСoef(perm);
+                if (k > maxK)
+                {
+                    maxK = k;
+                    result = (T[])perm.Clone();
+                }
+            });
+
+            _time?.Stop();
+
+            return result;
+        }
+    }
+}
---
+++ TravellingSalesmanProblem/IExhaustive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravellingSalesmanProblem
+{
+    // An interface to solve a task by exhaustive search
+    interface IExhaustive<T>
+    {
+        // Gets set over which the algorithm will work
+        T[] GetSet();
+
+        // If the permutation is more optimal than the function
+        // must return the higher сoefficient
+        int OptimalСoef(T[] perm);
+    }
+}
