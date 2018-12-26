8ddc831afb0718908c79cffd3123f88520014013
Qerny <sasha.kogut.98@mail.ru>
2018-12-27 00:55:20 +0300

    added interface ITimeCheck and class CheckTime to time tracking

---
+++ TravellingSalesmanProblem/ITimeCheck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravellingSalesmanProblem
+{
+    /// <summary>
+    /// An interface to note the time
+    /// </summary>
+    interface ITimeCheck
+    {
+        /// <summary>
+        /// Starts the count
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Restarts the count
+        /// </summary>
+        void Restart();
+
+        /// <summary>
+        /// Stops the count
+        /// </summary>
+        void Stop();
+
+        /// <summary>Gets the time in miliseconds</summary>
+        /// <returns></returns>
+        long GetTime();
+    }
+}
